59c257635cdc6f59bba52b27a5985f112e118f0b
tomazinhal <tomazinhal@gmail.com>
2017-05-02 17:44:08 +0100

    initial commit

---
+++ flask_helloworld.py
@@ -0,0 +1,105 @@
+from flask_oauthlib.client import OAuth, OAuthException
+from flask import Flask, jsonify, abort, make_response, redirect, url_for, session, request
+import requests as req
+
+"""
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+"""
+OAUTH_OKEN = "BQBZ_rpCDzkahYalrjeO_L1gXjztJyFrMsidAvDb5dl2HjLq4nqqmV-4kLhvAMKvMpL7sex0QmEfO8nnryyXAqDOsK25PTbHuuJLqTbdCFlkp--Rvjd6LH7qGHgeeojkXKRoEyh3c6Ao-DdqChwzlD4FQ2OuERCGbRdV"
+tomazinhal_url = "https://api.spotify.com/v1/users/11122241033"
+
+SPOTIFY_APP_ID = '9efc6cfd070049e18043aee96ae5228a'
+SPOTIFY_APP_SECRET = 'b6eea41f283446c78b0dbee02e3df4b7'
+
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development'
+oauth = OAuth(app)
+
+spotify = oauth.remote_app(
+    'spotify',
+    consumer_key=SPOTIFY_APP_ID,
+    consumer_secret=SPOTIFY_APP_SECRET,
+    # Change the scope to match whatever it us you need
+    # list of scopes can be found in the url below
+    # https://developer.spotify.com/web-api/using-scopes/
+    request_token_params={'scope': 'user-read-email'},
+    base_url='https://accounts.spotify.com',
+    request_token_url=None,
+    access_token_url='/api/token',
+    authorize_url='https://accounts.spotify.com/authorize'
+)
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    callback = url_for(
+        'spotify_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True
+    )
+    return spotify.authorize(callback=callback)
+
+
+@app.route('/login/authorized')
+def spotify_authorized():
+    resp = spotify.authorized_response()
+    if resp is None:
+        return 'Access denied: reason={0} error={1}'.format(
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    if isinstance(resp, OAuthException):
+        return 'Access denied: {0}'.format(resp.message)
+
+    session['oauth_token'] = (resp['access_token'], '')
+    me = spotify.get('/me')
+    return 'Logged in as id={0} name={1} redirect={2}'.format(
+        me.data['id'],
+        me.data['name'],
+        request.args.get('next')
+    )
+
+
+@spotify.tokengetter
+def get_spotify_oauth_token():
+    return session.get('oauth_token')
+
+
+##########################################################################################
+if __name__ == "__main__":
+    app.run()
